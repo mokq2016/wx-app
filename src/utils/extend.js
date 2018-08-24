c1969c9faaf10dce3d379f37b6154c4d59f66511
mkq <873302396@qq.com>
2018-08-24 18:20:26 +0800

    update

--- src/utils/extend.js
+++ src/utils/extend.js
@@ -6,11 +6,27 @@ wepy.component.prototype.$post = http.reqPost;
 
 
 wepy.component.prototype.$error = function(obj){
-    console.log(typeof obj === 'string',obj)
     if(typeof obj === 'string'){
         this.$invoke('message','handleShow',{content:obj})
     }else{
         this.$invoke('message','handleShow',obj)
     }
     
+}
+
+wepy.component.prototype.$alert = function(obj){
+    if(typeof obj === 'string'){
+        wx.showModal({
+            title:'提示',
+            content:obj,
+            showCancel:false
+        })
+    }else{
+        wx.showModal({
+            title:'提示',
+            content:obj,
+            showCancel:false,
+            ...obj
+        })
+    }
 }
